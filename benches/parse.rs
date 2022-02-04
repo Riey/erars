15f5e51a21e30d66330002707ef4a10ab0ea6004
Riey <creeper844@gmail.com>
2022-02-04 16:18:22 +0900

    Add parse_long bench

--- benches/parse.rs
+++ benches/parse.rs
@@ -12,5 +12,18 @@ fn parse_small(c: &mut Criterion) {
     });
 }
 
-criterion_group!(parse_benches, parse_small);
+fn parse_long(c: &mut Criterion) {
+    let mut code = String::with_capacity(100_000 * 30);
+    code.push_str("@FUNC\n");
+    for _ in 0..33333 {
+        code.push_str("IF A:1");
+        code.push_str("A:1 = A:2");
+        code.push_str("ENDIF");
+    }
+    c.bench_function("long 100000", |b| {
+        b.iter(|| compile(&code, &mut FunctionDic::new()).unwrap());
+    });
+}
+
+criterion_group!(parse_benches, parse_small, parse_long);
 criterion_main!(parse_benches);
