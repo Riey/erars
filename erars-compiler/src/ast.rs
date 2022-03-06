3dc4aa2a819abc3262c6b902cb9fd509417a91ff
Riey <creeper844@gmail.com>
2022-03-06 16:26:38 +0900

    Add some convenient expr methods

--- erars-compiler/src/ast.rs
+++ erars-compiler/src/ast.rs
@@ -33,3 +33,29 @@ pub enum Expr {
     BinopExpr(Box<Self>, BinaryOperator, Box<Self>),
     CondExpr(Box<Self>, Box<Self>, Box<Self>),
 }
+
+impl Expr {
+    pub fn int(i: impl Into<i64>) -> Self {
+        Self::IntLit(i.into())
+    }
+
+    pub fn str(s: impl Into<String>) -> Self {
+        Self::StringLit(s.into())
+    }
+
+    pub fn var(n: impl Into<String>, args: Vec<Self>) -> Self {
+        Self::Var(n.into(), args)
+    }
+
+    pub fn unary(op1: Self, op: UnaryOperator) -> Self {
+        Self::UnaryopExpr(Box::new(op1), op)
+    }
+
+    pub fn binary(op1: Self, op: BinaryOperator, op2: Self) -> Self {
+        Self::BinopExpr(Box::new(op1), op, Box::new(op2))
+    }
+
+    pub fn cond(op1: Self, op2: Self, op3: Self) -> Self {
+        Self::CondExpr(Box::new(op1), Box::new(op2), Box::new(op3))
+    }
+}
