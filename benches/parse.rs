e400c2ec1803c75c7dba6596054e73125ac66860
Riey <creeper844@gmail.com>
2022-04-02 01:59:09 +0900

    Update bench code

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,22 +1,24 @@
 use criterion::*;
 use erars::erars_compiler::parse_program;
+use erars_compiler::VariableInterner;
 
 fn parse_small(c: &mut Criterion) {
+    let var = VariableInterner::with_default_variables();
     c.bench_function("small 5", |b| {
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(5);
-        b.iter(|| parse_program(&code).unwrap());
+        b.iter(|| parse_program(&code, &var).unwrap());
     });
     c.bench_function("small 500", |b| {
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(500);
-        b.iter(|| parse_program(&code).unwrap());
+        b.iter(|| parse_program(&code, &var).unwrap());
     });
     c.bench_function("small 5000", |b| {
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(5000);
-        b.iter(|| parse_program(&code).unwrap());
+        b.iter(|| parse_program(&code, &var).unwrap());
     });
     c.bench_function("small 50000", |b| {
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(50000);
-        b.iter(|| parse_program(&code).unwrap());
+        b.iter(|| parse_program(&code, &var).unwrap());
     });
 }
 
