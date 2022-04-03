f557e7561fbb87fd170196b3b91d7f71b9aedf16
Riey <creeper844@gmail.com>
2022-04-03 15:39:32 +0900

    Bench real erb files

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,5 +1,5 @@
 use criterion::*;
-use erars::erars_compiler::parse_program;
+use erars::erars_compiler::{compile, parse_program};
 use erars_compiler::VariableInterner;
 
 fn parse_small(c: &mut Criterion) {
@@ -22,5 +22,43 @@ fn parse_small(c: &mut Criterion) {
     });
 }
 
-criterion_group!(parse_benches, parse_small);
-criterion_main!(parse_benches);
+fn parse_real(c: &mut Criterion) {
+    let var = VariableInterner::with_default_variables();
+
+    c.bench_function("parse title", |b| {
+        b.iter(|| parse_program(include_str!("../ERB/TITLE.ERB"), &var));
+    });
+
+    c.bench_function("parse system", |b| {
+        b.iter(|| parse_program(include_str!("../ERB/SYSTEM.ERB"), &var));
+    });
+}
+
+fn compile_real(c: &mut Criterion) {
+    let var = VariableInterner::with_default_variables();
+
+    c.bench_function("compile title", |b| {
+        b.iter(|| {
+            parse_program(include_str!("../ERB/TITLE.ERB"), &var)
+                .unwrap()
+                .into_iter()
+                .map(|f| compile(f, &var))
+                .collect::<Result<Vec<_>, _>>()
+        });
+    });
+
+    c.bench_function("compile system", |b| {
+        b.iter(|| {
+            parse_program(include_str!("../ERB/SYSTEM.ERB"), &var)
+                .unwrap()
+                .into_iter()
+                .map(|f| compile(f, &var))
+                .collect::<Result<Vec<_>, _>>()
+        });
+    });
+}
+
+criterion_group!(parse_benches, parse_small, parse_real);
+criterion_group!(compile_benches, compile_real);
+
+criterion_main!(parse_benches, compile_benches);
