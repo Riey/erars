760ecab0f96c0d687aa5f9a3331fc5855e4faa79
Riey <creeper844@gmail.com>
2022-04-24 19:05:24 +0900

    Organize bench code

--- benches/parse.rs
+++ benches/parse.rs
@@ -3,46 +3,42 @@ use erars::erars_compiler::{compile, parse_program};
 use erars_compiler::VariableDic;
 
 fn parse_small(c: &mut Criterion) {
+    let var = VariableDic::default();
     c.bench_function("small 5", |b| {
-        let var = VariableDic::default();
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(5);
         b.iter(|| parse_program(&code, &var).unwrap());
     });
     c.bench_function("small 500", |b| {
-        let var = VariableDic::default();
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(500);
         b.iter(|| parse_program(&code, &var).unwrap());
     });
     c.bench_function("small 5000", |b| {
-        let var = VariableDic::default();
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(5000);
         b.iter(|| parse_program(&code, &var).unwrap());
     });
     c.bench_function("small 50000", |b| {
-        let var = VariableDic::default();
         let code = "@FUNC\nPRINTL Hello, world!\n".repeat(50000);
         b.iter(|| parse_program(&code, &var).unwrap());
     });
 }
 
 fn parse_real(c: &mut Criterion) {
+    let var = VariableDic::default();
     c.bench_function("parse title", |b| {
-        let var = VariableDic::default();
         b.iter(|| parse_program(include_str!("../ERB/TITLE.ERB"), &var));
     });
 
     c.bench_function("parse system", |b| {
-        let var = VariableDic::default();
         b.iter(|| parse_program(include_str!("../ERB/SYSTEM.ERB"), &var));
     });
 }
 
 fn compile_real(c: &mut Criterion) {
-    let mut var = VariableDic::default();
+    let var = VariableDic::default();
 
     c.bench_function("compile title", |b| {
         b.iter(|| {
-            parse_program(include_str!("../ERB/TITLE.ERB"), &mut var)
+            parse_program(include_str!("../ERB/TITLE.ERB"), &var)
                 .unwrap()
                 .into_iter()
                 .map(|f| compile(f, &var))
@@ -52,7 +48,7 @@ fn compile_real(c: &mut Criterion) {
 
     c.bench_function("compile system", |b| {
         b.iter(|| {
-            parse_program(include_str!("../ERB/SYSTEM.ERB"), &mut var)
+            parse_program(include_str!("../ERB/SYSTEM.ERB"), &var)
                 .unwrap()
                 .into_iter()
                 .map(|f| compile(f, &var))
