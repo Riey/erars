74c09bdb62fac57dcbd9e85c76a41c545e5c8780
Riey <creeper844@gmail.com>
2022-09-24 14:52:41 +0900

    Compress save data

--- src/vm/save_data.rs
+++ src/vm/save_data.rs
@@ -1,3 +1,5 @@
+use flate2::read;
+use flate2::write;
 use std::path::Path;
 
 use super::{
@@ -10,27 +12,30 @@ pub fn write_save_data(sav_path: &Path, idx: i64, var: &VariableStorage, descrip
         std::fs::create_dir(&sav_path).ok();
     }
 
-    std::fs::write(
-        sav_path.join(format!("save{idx:02}.msgpack")),
-        &rmp_serde::to_vec(&var.get_serializable(description)).unwrap(),
-    )
-    .unwrap();
+    let mut file =
+        std::fs::File::create(sav_path.join(format!("save{idx:02}.msgpack.gz"))).unwrap();
+    let mut encoder = write::GzEncoder::new(&mut file, flate2::Compression::fast());
+
+    rmp_serde::encode::write(&mut encoder, &var.get_serializable(description)).unwrap();
 }
 
 pub fn read_save_data(sav_path: &Path, idx: i64) -> Result<SerializableVariableStorage, i64> {
-    let file = sav_path.join(format!("save{idx:02}.msgpack"));
+    let file = sav_path.join(format!("save{idx:02}.msgpack.gz"));
 
     let ret = if !file.exists() {
         1
     } else {
-        match std::fs::read(file) {
-            Ok(s) => match rmp_serde::from_read::<_, SerializableVariableStorage>(s.as_slice()) {
-                Ok(ret) => return Ok(ret),
-                Err(err) => {
-                    log::error!("Save load error: {err}");
-                    4
+        match std::fs::File::open(file) {
+            Ok(compressed) => {
+                let mut decoder = read::GzDecoder::new(compressed);
+                match rmp_serde::from_read::<_, SerializableVariableStorage>(&mut decoder) {
+                    Ok(ret) => return Ok(ret),
+                    Err(err) => {
+                        log::error!("Save load error: {err}");
+                        4
+                    }
                 }
-            },
+            }
             Err(err) => {
                 log::error!("File read error: {err}");
                 4
@@ -46,6 +51,7 @@ pub fn write_global_data(sav_path: &Path, var: &VariableStorage) {
         std::fs::create_dir(&sav_path).ok();
     }
 
+    // Don't compress global data since it's pretty small
     std::fs::write(
         sav_path.join(format!("global.msgpack")),
         &rmp_serde::to_vec(&var.get_global_serializable()).unwrap(),
