eb0c749384c3f8a3013d24bfcf470dd56c596a8e
Riey <creeper844@gmail.com>
2023-01-06 20:38:50 +0900

    Remove unused dbg!

--- crates/erars-lexer/src/lib.rs
+++ crates/erars-lexer/src/lib.rs
@@ -356,8 +356,6 @@ impl<'s> Preprocessor<'s> {
 
         debug_assert!(!line.ends_with('\n'));
 
-        dbg!(line);
-
         let line = if let Some(pos) = memchr::memchr(b';', line.as_bytes()) {
             if line.starts_with("PRINT") && !line.starts_with(&['V', 'S'])
                 || line.starts_with("REUSELASTLINE")
@@ -369,7 +367,6 @@ impl<'s> Preprocessor<'s> {
         } else {
             line
         };
-        dbg!(line);
 
         if line.is_empty() {
             Ok(None)
