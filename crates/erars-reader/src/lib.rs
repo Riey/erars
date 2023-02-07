7517e1707b4c62874d45b69e75904efc7397a20a
Riey <creeper844@gmail.com>
2023-02-08 02:31:59 +0900

    Read file via mmap

--- crates/erars-reader/src/lib.rs
+++ crates/erars-reader/src/lib.rs
@@ -1,50 +1,25 @@
-use std::io::{BufRead, BufReader, Read, Seek, SeekFrom};
+use memmap2::MmapOptions;
 use std::path::Path;
 
 /// Read string from path respect BOM
 pub fn read_file(path: &Path) -> std::io::Result<String> {
-    let mut file = std::fs::File::open(path)?;
+    let file = std::fs::File::open(path)?;
+    let src = unsafe { MmapOptions::new().populate().map(&file) }.unwrap();
+    let mut src = &*src;
 
     use unicode_bom::Bom;
 
-    let bom = Bom::from(&mut file);
-    file.seek(SeekFrom::Start(bom.len() as _))?;
+    let bom = Bom::from(src);
+    src = &src[bom.len()..];
     let encoding = match bom {
         Bom::Null => encoding_rs::SHIFT_JIS,
         Bom::Utf16Be => encoding_rs::UTF_16BE,
         Bom::Utf16Le => encoding_rs::UTF_16LE,
-        Bom::Utf8 => {
-            let mut out = String::with_capacity(file.metadata()?.len() as usize);
-            file.read_to_string(&mut out)?;
-            return Ok(out);
-        }
+        Bom::Utf8 => unsafe {
+            return Ok(std::str::from_utf8_unchecked(src).to_string());
+        },
         other => panic!("Unsupported BOM {other}"),
     };
 
-    let mut decoder = encoding.new_decoder();
-
-    let len = decoder
-        .max_utf8_buffer_length(file.metadata()?.len() as usize)
-        .expect("File is too large");
-
-    let mut reader = BufReader::new(file);
-    let mut out = String::with_capacity(len);
-
-    loop {
-        let src = reader.fill_buf()?;
-
-        if src.is_empty() {
-            break;
-        }
-
-        let (_ret, size, had_errors) = decoder.decode_to_string(src, &mut out, false);
-
-        if had_errors {
-            log::error!("Invalid text detected from {}", path.display());
-        }
-
-        reader.consume(size);
-    }
-
-    Ok(out)
+    Ok(encoding.decode_without_bom_handling(src).0.to_string())
 }
